b48b01086b584b73d40be6e87a31faa830ac8549
Strenger <leon.strenger@gmail.com>
2026-05-11 12:50:35 +0200

    update packaging info

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="pytrance",
     version="0.1.0",
-    description="pyTrance: utilities for subcellular spatial transcriptomics analysis",
+    description="pyTrance: framework for subcellular spatial transcriptomics analysis",
     author="",
     author_email="",
     packages=find_packages(include=["pytrance", "pytrance.*"]),
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
         "numpy",
         "tqdm",
