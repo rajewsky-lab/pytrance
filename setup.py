f578d15817c000b565bf3e7d9faf0f1127305ed9
Strenger <leon.strenger@gmail.com>
2026-09-09 15:49:31 +0200

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytrance",
-    version="0.1.0",
+    version="0.2.0",
     description="pyTrance: framework for subcellular spatial transcriptomics analysis",
     author="",
     author_email="",
